b365f746d3e3c49ebbc4e088ea997d6972c0f4a9
Aon1840 <aonattapon1840.ap@gmail.com>
2021-11-29 14:20:11 +0700

    Add common set state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
-function ExpenseItem(props) {    
+const ExpenseItem = (props) => {  
+    const [title, setTitle] = useState(props.expenseTitle)
+    console.log("ExpensesItem evaluated by React")
+    const clickHandler = () => {
+        setTitle("Updated!")
+        console.log(title)
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate
                 expenseDate = {props.expenseDate}
             />
             <div className="expense-item__description">
-                <h2>{props.expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.expenseAmount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
